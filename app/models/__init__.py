39359d9583bdbacc21507f9a95dc9e0aa5fad425
Shyam Santoki <shyam_santoki@outlook.com>
2023-03-02 12:00:59 +0530

    Updated import file references

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,6 @@
+from configs.database import SQLALCHEMY_URL
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-
-from app.configs.database import SQLALCHEMY_URL
 
 engine = create_engine(SQLALCHEMY_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
